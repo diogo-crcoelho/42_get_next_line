ff68363da9652163b03fb3121f59aaf471ae2fec
Diogo Carvalho Da Rocha Coelho <dcarvalh@c1r4s1.42lisboa.com>
2022-11-04 17:24:29 +0000

    2

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 22:02:00 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/11/04 12:39:04 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/11/04 16:15:57 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "get_next_line.h"
 
-char 	get_next_line(int fd)
+char 	*get_next_line(int fd)
 {
 	static char *stash;
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,17 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 20:54:03 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/11/04 12:38:36 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/11/04 17:23:49 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE=42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
-void	print_size();
 
 #endif
