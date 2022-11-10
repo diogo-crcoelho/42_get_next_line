6b5bc5a599c6833da50a55d617aea4423c313a33
dcarvalh <dcarvalh@student.42lisboa.com>
2022-11-10 15:44:20 +0000

    checker

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:09:05 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/11/09 16:28:13 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/11/10 15:43:31 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ char	*get_line(char *line, char **stash)
 	ft_memcpy(&new[i], "\n", 2);
 	while (i-- > 0)
 	 	new[i] = line[i];
+	free (line);
 	return (new);
 }
 
@@ -61,6 +62,7 @@ char	*ft_read(int fd, char **stash)
 		if (ft_strchr(temp, '\n'))
 			return (get_line(line, stash));
 	}
+	free(*stash);
 	*stash = ft_strdup(line);
 	free (line);
 	return (NULL);	
@@ -76,6 +78,7 @@ char	*get_next_line(int fd)
 	line = (char *)malloc(BUFFER_SIZE + 1);
 	if (!line)
 		return (NULL);
+	free (line);
 	line = ft_read(fd, &stash);
 	if (line)
 		return (line);	
@@ -83,6 +86,7 @@ char	*get_next_line(int fd)
 	stash = NULL;
 	if (*line)
 		return (line);
+	free(line);
 	return (NULL);
 	// printf("-%s\n", line);
 }
@@ -99,9 +103,9 @@ int main()
 	// temp = get_next_line(fd);
 	// printf("%s\n", temp);
 	while ((temp = get_next_line(fd)) != NULL)
-	// for(int i = 0; i < 10; i ++)
+	// for(int i = 0; i < 3; i ++)
 	{
-//		temp = get_next_line(fd);
+		// temp = get_next_line(fd);
 		printf("%s",temp);
 		free(temp);
 	}
