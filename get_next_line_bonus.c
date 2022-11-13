3d10544adedf507f384eeb9bb5edd10a72ee403d
Diogo Carvalho Da Rocha Coelho <dcarvalh@c2r4s3.42lisboa.com>
2022-11-13 15:05:03 +0000

    Bonus e norm

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,36 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:09:05 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/11/13 14:35:07 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/11/13 15:03:15 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include "get_next_line.h"
+#include <stdio.h>
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	stash[BUFFER_SIZE + 1];
+	static char	stash[FOPEN_MAX][BUFFER_SIZE + 1];
 	size_t		i;
 
 	i = -1;
-	if (read(fd, 0, 0) < 0 || BUFFER_SIZE < 1)
+	if (read(fd, 0, 0) < 0 || BUFFER_SIZE < 1 || fd >= FOPEN_MAX)
 	{	
-		while (stash[++i])
-			stash[i] = 0;
+		if (fd > 0 && fd < FOPEN_MAX)
+			while (stash[fd][++i])
+				stash[fd][i] = 0;
 		return (NULL);
 	}
 	line = NULL;
-	while (*stash || read(fd, stash, BUFFER_SIZE) > 0)
+	while (*(stash[fd]) || read(fd, stash[fd], BUFFER_SIZE) > 0)
 	{
-		line = ft_strjoin(line, stash);
-		if (next_line(stash) != -1)
+		line = ft_strjoin(line, stash[fd]);
+		if (next_line(stash[fd]) != -1)
 			break ;
 	}
 	return (line);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 23:52:35 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/11/13 14:34:56 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/11/13 14:35:35 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
